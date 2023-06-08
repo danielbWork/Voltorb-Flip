56b3df2a9ed3e89b62599aa2da3fb3ebe45c5950
danielbwork <danielbwork12@gmail.com>
2023-06-08 16:42:31 +0300

    FIxed parcler bugs and started ui

---
+++ src/browserAction/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  let test = "Hello world";
+</script>
+
+<div>
+  {test}
+</div>
+
+<style>
+  div {
+    height: 600px;
+    width: 500px;
+    padding: 8px;
+    background-color: #309f6a;
+  }
+</style>
