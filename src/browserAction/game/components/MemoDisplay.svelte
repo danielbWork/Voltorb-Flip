23a10d4201fdd576356d109b1b17c3cc644b2b4a
danielbwork <danielbwork12@gmail.com>
2023-06-29 12:35:33 +0300

    Updated memo ui to look good

--- src/browserAction/game/components/MemoDisplay.svelte
+++ src/browserAction/game/components/MemoDisplay.svelte
@@ -20,14 +20,35 @@
   function onToggleMemo() {
     dispatch("memoToggled");
   }
+
+  const checkedImages = [
+    "memo_checked_0",
+    "memo_checked_1",
+    "memo_checked_2",
+    "memo_checked_3",
+  ];
+  const uncheckedImages = [
+    "memo_unchecked_0",
+    "memo_unchecked_1",
+    "memo_unchecked_2",
+    "memo_unchecked_3",
+  ];
+
+  // const
 </script>
 
 {#if isMemoOpen}
   <div class="container" transition:fly={{ x: 50, duration: 500 }}>
-    <input type="checkbox" bind:checked={memos[0]} on:change={onToggleMemo} />
-    <input type="checkbox" bind:checked={memos[1]} on:change={onToggleMemo} />
-    <input type="checkbox" bind:checked={memos[2]} on:change={onToggleMemo} />
-    <input type="checkbox" bind:checked={memos[3]} on:change={onToggleMemo} />
+    {#each memos as memo, index}
+      <input
+        type="checkbox"
+        bind:checked={memo}
+        on:change={onToggleMemo}
+        style={`background-image: url("/icons/${
+          memo ? checkedImages[index] : uncheckedImages[index]
+        }.png");`}
+      />
+    {/each}
   </div>
 {/if}
 
@@ -36,7 +57,7 @@
   .container {
     height: 68px;
     width: 68px;
-    margin-top: -20px;
+    margin-top: -25px;
     margin-left: 386px;
     outline: 3px solid #404040;
     border: 3px solid #906860;
@@ -45,11 +66,20 @@
     display: grid;
     grid-template-columns: auto auto;
     gap: 0px;
+    padding: 2px;
   }
 
-  /* .memo-button-shown {
+  input[type="checkbox"] {
+    appearance: none;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: contain;
+    margin: 0px;
+    padding: 0px;
+    background-color: #88a8a8;
   }
 
-  .memo-button-disabled {
-  } */
+  input[type="checkbox"]:checked {
+    background-color: #188060;
+  }
 </style>
